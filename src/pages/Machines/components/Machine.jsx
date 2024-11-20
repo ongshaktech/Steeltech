49a6729c847b82ab8e010c34c6282c849642525b
Shahjalal Rafi <shahjalalrafi5@gmail.com>
2024-11-20 08:52:17 +0600

    Machine add

--- src/pages/Machines/components/Machine.jsx
+++ src/pages/Machines/components/Machine.jsx
@@ -14,6 +14,11 @@ export default function Machine(props) {
   //   export default function Machine({ meta, machineNo, active, idleTime }) {
   let [totalProduct, setTotalProduct] = useState(0);
   let [totalWeight, setTotalWeight] = useState(0);
+  let [product, setProduct] = useState({
+    product_type: "",
+    thickness: "",
+    product_dimensions: "",
+  });
 
   useEffect(() => {
     let collectionRef = collection(db_firestore, "machines");
@@ -30,10 +35,17 @@ export default function Machine(props) {
         ...doc.data(),
       }));
 
+      console.log("updatedData", updatedData);
+
+      setProduct({
+        product_type: updatedData[0]?.product_type,
+        thickness: updatedData[0]?.thickness,
+        product_dimensions: updatedData[0]?.product_dimensions
+      });
       setTotalProduct(updatedData[0]?.count ? updatedData[0]?.count : 0);
-        setTotalWeight(
-          updatedData[0]?.weight == "" || NaN || null ? 0 : updatedData[0]?.weight
-        );
+      setTotalWeight(
+        updatedData[0]?.weight == "" || NaN || null ? 0 : updatedData[0]?.weight
+      );
 
       // snapShot.forEach((doc) => {
       //   setTotalProduct(doc.data().count ? doc.data().count : 0);
@@ -46,15 +58,14 @@ export default function Machine(props) {
     return () => unsubscribe();
   }, []);
 
-
   return (
     <div
-      className={`border-[3px] p-4 rounded-lg ${
+      className={`border-[3px]  rounded-lg ${
         props?.active ? "border-[#009245]" : "border-[#ED1C24]"
       }`}
     >
-      <div className="flex gap-4 justify-between items-center">
-        <div className="flex flex-col gap-2">
+      <div className="flex gap-4 justify-between h-full ">
+        <div className="w-full flex flex-col gap-2 justify-between p-3">
           <h2 className="text-2xl font-semibold">FM {props.machineNo}</h2>
           <button className="flex gap-2 items-center bg-black text-white px-4 py-2 rounded-md">
             <div
@@ -64,15 +75,21 @@ export default function Machine(props) {
             ></div>
             <p>{props?.active ? "Active" : "Idle"}</p>
           </button>
+
+          <div className="pt-3">
+            <p>(Last updated {props.idleTime} ago)</p>
+            <p>Total Product: {totalProduct}</p>
+            <p>Total Weight: {totalWeight} kg</p>
+          </div>
         </div>
-        <div className="">
-          <img src={Machine_Logo} alt="" />
+        <div className="min-w-[90px]  bg-black/80 text-white flex flex-col justify-between items-center  px-2 py-6">
+          <img src={Machine_Logo} alt="" className="w-[60px] h-auto" />
+          <div className="text-center text-xs">
+            <p>{product?.product_type}</p>
+            <p>{product?.thickness}</p>
+            <p>{product?.product_dimensions}</p>
+          </div>
         </div>
-      </div>
-      <div className="pt-3">
-        <p>(Last updated {props.idleTime} ago)</p>
-        <p>Total Product: {totalProduct}</p>
-        <p>Total Weight: {totalWeight} kg</p>
       </div>
     </div>
   );
