0ac3fa8c09b8eccc463ed988af26820eba3932bb
Shahjalal Rafi <shahjalalrafi5@gmail.com>
2024-11-20 16:11:21 +0600

    All machine add

--- src/pages/AllMachine/AllMachine.jsx
+++ src/pages/AllMachine/AllMachine.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import { arrayUnion, doc, onSnapshot, updateDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db_firestore } from "../../Hooks/config";
+import { CiCirclePlus } from "react-icons/ci";
+import AddMachine from "./components/AddMachine";
 
 export default function AllMachine() {
+  const [showAdd, setShowAdd] = useState(false);
+  let [formingMachines, setFormingMachines] = useState([]);
+
+  //  *********************************** Update Data **********************************
+
+  // useEffect(() => {
+  //     const ref = doc(db_firestore, `information`, 'info');
+
+  //     if (!(Object.keys(polishMachineData).length === 0 && polishMachineData.constructor === Object)) {
+  //         updateDoc(ref, {
+  //             polish_machine: arrayUnion(polishMachineData['machine_no'].toString())
+  //         });
+  //     }
+  // }, [polishMachineData]);
+
+  // **************************** Fetch and insert data to div **************************
+  useEffect(() => {
+    const ref = doc(db_firestore, `information`, "info");
+
+    // getDoc(ref).then(
+    onSnapshot(ref, (data) => {
+      const machines = data.data();
+      let forming_machines = [];
+      let polish_machines = [];
+
+      machines["forming_machine"].forEach((num, index) => {
+        forming_machines.push(num);
+      });
+
+      // machines['polish_machine'].forEach((num, index) => {
+      //     polish_machines.push(
+      //         <div className={style.listElements} key={index}>
+      //             <span>{num}</span>
+      //         </div>
+      //     );
+      // });
+
+      setFormingMachines(forming_machines);
+      // setPolishMachines(polish_machines);
+    });
+  }, []);
+
   return (
-    <div>AllMachine</div>
-  )
+    <div>
+      <h2 className="text-3xl font-bold py-6">Forming Machines</h2>
+      <div className="flex justify-between items-center gap-4 mb-8 mr-10">
+        <h2>List of Machines</h2>
+        <button
+          className="bg-primary px-4 py-2 rounded-full text-white flex gap-2 items-center"
+          onClick={() => {
+            setShowAdd(!showAdd);
+          }}
+        >
+          Add Product
+          <CiCirclePlus className="w-6 h-6" />
+        </button>
+      </div>
+      {showAdd ? <AddMachine setShowAdd={setShowAdd} /> : null}
+
+      <div className="flex flex-wrap gap-4 items-center">
+        {formingMachines?.map((machine) => (
+          <div className="px-6 py-2 rounded-xl border border-black">
+            FM {machine}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/AllMachine/components/AddMachine.jsx
@@ -0,0 +1,52 @@
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db_firestore } from "../../../Hooks/config";
+
+export default function AddMachine({ setShowAdd }) {
+  let [msg, setMsg] = useState("");
+  let [machine_no, setMachine_no] = useState(null);
+
+  const setData = (e) => {
+    e.preventDefault();
+
+    if (machine_no !== "") {
+     
+      const ref = doc(db_firestore, `information`, "info");
+
+      updateDoc(ref, {
+        forming_machine: arrayUnion(machine_no),
+      });
+      setMachine_no(null);
+      setShowAdd(false)
+    } else {
+      setMsg("Machine number cannot be empty!");
+      return false;
+    }
+  };
+
+  return (
+    <form className="py-10" onSubmit={setData}>
+      <h2 className="text-2xl pb-4 font-bold">Add Machine</h2>
+
+      <label className="flex gap-6 items-center mb-6">
+        <p className="w-[180px]">Machine No*</p>
+        <input
+          id=""
+          type="number"
+          name="machine_no"
+          className="w-full p-2 rounded-md border border-black outline-none focus:outline-none"
+          value={machine_no}
+          onChange={(e) => setMachine_no(e.target.value)}
+        />
+      </label>
+
+      {msg ? <span className="text-red-500">{msg}</span> : null}
+      <div className="flex justify-center">
+
+      <button className="btn-primary" type="submit">
+        Add
+      </button>
+      </div>
+    </form>
+  );
+}
