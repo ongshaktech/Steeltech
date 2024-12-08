e514319a7a30a465b696e5593e513f20f9d37457
Shahjalal Rafi <shahjalalrafi5@gmail.com>
2024-12-08 11:01:51 +0600

    overview analytics update

--- src/pages/Overview/components/MachineOverview.jsx
+++ src/pages/Overview/components/MachineOverview.jsx
@@ -3,6 +3,8 @@ import { FaArrowTrendUp } from "react-icons/fa6";
 import active_machine_analytics from "../../../assets/images/active_machine_analytics.svg";
 import idle_machine_analytics from "../../../assets/images/idle_machine_analytics.svg";
 import total_machine_analytics from "../../../assets/images/total_machine_analytics.svg";
+
+import Machine_Logo from "../../../assets/images/Forming_Machine.svg";
 import {
   collection,
   doc,
@@ -119,36 +121,37 @@ export default function MachineOverview() {
       <div className="bg-white shadow-[5px_5px_20px_1px_rgba(0,0,0,0.2)] w-full p-4 rounded-lg flex flex-col gap-6">
         <div className="flex gap-4 justify-between items-center">
           <p>Active Machines</p>
-          <div className="flex gap-2 items-center text-[#2CF619]">
+          {/* <div className="flex gap-2 items-center text-[#2CF619]">
             <FaArrowTrendUp className="fill-[#2CF619]" />
             <p>+17%</p>
-          </div>
+          </div> */}
         </div>
         <div className="flex gap-4 items-end  justify-between">
           <h2>
             <span className="text-2xl font-bold">{activeMachine}</span> Machines
             Active
           </h2>
-          <img src={active_machine_analytics} alt="" />
+          {/* <img src={Machine_Logo} alt="" /> */}
+          <img src={Machine_Logo} alt="" className="w-[60px] h-auto " />
         </div>
       </div>
       <div className="bg-white shadow-[5px_5px_20px_1px_rgba(0,0,0,0.2)] w-full p-4 rounded-lg flex flex-col gap-6">
         <div className="flex gap-4 justify-between items-center">
           <p>Idle Machines</p>
-          <div className="flex gap-2 items-center text-[#ED1C24]">
+          {/* <div className="flex gap-2 items-center text-[#ED1C24]">
             <FaArrowTrendUp className="fill-[#ED1C24]" />
             <p>+17%</p>
-          </div>
+          </div> */}
         </div>
         <div className="flex gap-4 items-end  justify-between">
           <h2>
             <span className="text-2xl font-bold">{inActiveMachine}</span>{" "}
             Machines Idle
           </h2>
-          <img src={idle_machine_analytics} alt="" />
+          <img src={Machine_Logo} alt="" className="w-[60px] h-auto " />
         </div>
       </div>
-      <div className="bg-white shadow-[5px_5px_20px_1px_rgba(0,0,0,0.2)] w-full p-4 rounded-lg flex flex-col gap-6">
+      {/* <div className="bg-white shadow-[5px_5px_20px_1px_rgba(0,0,0,0.2)] w-full p-4 rounded-lg flex flex-col gap-6">
         <div className="flex gap-4 justify-between items-center">
           <p>Total Current Production</p>
           <div className="flex gap-2 items-center text-[##16B1FF]">
@@ -162,7 +165,7 @@ export default function MachineOverview() {
           </h2>
           <img src={total_machine_analytics} alt="" />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
